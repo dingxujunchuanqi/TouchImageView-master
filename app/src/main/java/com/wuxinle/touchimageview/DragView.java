9e4b157c9d0126853f19cb103ce6ab2affb59f18
dxunjun <849336182@qq.com>
2018-05-04 09:32:05 +0800

    随手指拖动的图片首次提交

---
+++ app/src/main/java/com/wuxinle/touchimageview/DragView.java
@@ -0,0 +1,94 @@
+package com.wuxinle.touchimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.wuxinle.touchimageview.utils.ScreenUtil;
+
+/**
+ * Created by dingxujun on 2018/4/27.
+ *
+ * @project TouchImageView-master
+ */
+
+public class DragView extends ImageView {
+    private Context context;
+    private float moveX;
+    private float moveY;
+    private float DownX;
+    private float DownY;
+    private float UpX;
+    private float UpY;
+    private DragViewOnclickListener mListener;
+
+    public DragView(Context context) {
+        super(context);
+    }
+
+    public DragView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+
+
+    /**
+     * 坐标说明 view.getX()坐标是点击的空间左上角相对父容器而言的
+     * event.getX();触摸点相对于其所在组件原点的坐标
+     * event.getRowX() 触摸点相对于屏幕原点的
+     *
+     * @date 创建时间 2018/4/27
+     * @author dingxujun
+     */
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+//获取相对屏幕的坐标(以屏幕左上角为原点,25是系统状态栏的高度)
+                moveX = event.getX();
+                moveY = event.getY();
+                DownX = event.getRawX();
+                DownY = event.getRawY() - 25;
+                setPressed(true);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (event.getRawX() > 50 && event.getRawX() < (ScreenUtil.getInstance(context).getScreenWidth() - 20)
+                        && event.getRawY() > 50 && event.getRawY() < (ScreenUtil.getInstance(context).getScreenHeight() - 100)) {
+                    setX(getX() + (event.getX() - moveX));
+                    setY(getY() + (event.getY() - moveY));
+
+                } else {
+                    setX((ScreenUtil.getInstance(context).getScreenWidth()) - getMeasuredWidth() - 20);
+                    setY((ScreenUtil.getInstance(context).getScreenHeight()) / 2);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                setPressed(false);
+                UpX = event.getRawX();
+                UpY = event.getRawY() - 25;
+                if (Math.abs(UpX - DownX) > 10 || Math.abs(UpY - DownY) > 10) {
+                    //处理滑动事件
+                    Toast.makeText(context, "滑动", 0).show();
+                } else {
+                    //处理点击事件
+                    mListener.dragViewOnClick();
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
+    }
+
+    public interface DragViewOnclickListener {
+        void dragViewOnClick();
+    }
+
+    public void setDragViewOnclickListener(DragViewOnclickListener listener) {
+        this.mListener = listener;
+
+    }
+}
---
+++ app/src/main/java/com/wuxinle/touchimageview/MainActivity.java
@@ -0,0 +1,65 @@
+package com.wuxinle.touchimageview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Toast;
+
+import com.wuxinle.touchimageview.utils.ScreenUtil;
+
+public class MainActivity extends Activity implements DragView.DragViewOnclickListener {
+
+    private DragView drag_imgage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drag_imgage = (DragView) findViewById(R.id.drag_imgage);
+        drag_imgage.setDragViewOnclickListener(this);
+//        measure2();
+        measure4();
+    }
+
+    private void measure2() {//比如一张图片，测量的是实际图片的高度,不管你有没有在布局文件中固定宽高
+        //手动调用测量方法。
+//制定测量规则 参数表示size + mode
+        int w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        drag_imgage.measure(w, h);
+//调用measure方法之后就可以获取宽高。
+        int height = drag_imgage.getMeasuredHeight();
+        int width = drag_imgage.getMeasuredWidth();
+
+        int with1 = (ScreenUtil.getInstance(this).getScreenWidth()) - width - 10;
+        int height1 = (ScreenUtil.getInstance(this).getScreenHeight()) / 2;
+
+        drag_imgage.setX(with1);
+        drag_imgage.setY(height1);
+    }
+    private void measure4() {//测量的是 你设置后的控件的宽高
+        ViewTreeObserver vto = drag_imgage.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                drag_imgage.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                Log.i("imageview4", "width: " + drag_imgage.getWidth());
+                Log.i("imageview4", "height: " + drag_imgage.getHeight());
+                int width1 = (ScreenUtil.getInstance(MainActivity.this).getScreenWidth()) - drag_imgage.getWidth() - 20;
+                int height1 = (ScreenUtil.getInstance(MainActivity.this).getScreenHeight()) / 2;
+
+                drag_imgage.setX(width1);
+                drag_imgage.setY(height1);
+            }
+        });
+
+    }
+
+    @Override
+    public void dragViewOnClick() {
+        Toast.makeText(this, "点击我了", 0).show();
+    }
+}
+
